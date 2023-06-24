9ca274e6890af56b89695cb7b92d081d95cd17b0
neo-astro <estevanabril@hotmail.es>
2023-06-24 16:18:09 -0500

    graf

--- xd.py
+++ xd.py
@@ -29,4 +29,39 @@ registro = {
 #         print(registro[dato][0][keyData][valor] )
 
 
-registro
+var = {
+  "data": [
+      {
+          "_id": "648e5b3d46448fa5c63d3828",
+          "fecha": "2023-10-10",
+          "senHumedadAire": {"1": 40,"2":50 },
+          "senHumedadAgua": {"1": 40,"2":50 },
+          "senPh": {"1": 40,"2":50 },
+          "senCalidadAire": {"1": 40,"2":50 }
+      },
+        {
+          "_id": "648e5b3d46448fa5c63d3828",
+          "fecha": "2023-10-12",
+          "senHumedadAire": {"1": 40,"2":50 },
+          "senHumedadAgua": {"1": 40,"2":50 },
+          "senPh": {"1": 40,"2":50 },
+          "senCalidadAire": {"1": 40,"2":50 }
+      },
+        {
+          "_id": "648e5b3d46448fa5c63d3828",
+          "fecha": "2023-10-11",
+          "senHumedadAire": {"1": 40,"2":50 },
+          "senHumedadAgua": {"1": 40,"2":50 },
+          "senPh": {"1": 40,"2":50 },
+          "senCalidadAire": {"1": 40,"2":50 }
+      }
+  ]
+}
+
+config = {
+    'distancia': [3,7,11,15,19,23,27,31,35,39,43,47,51,55,59,63,67,71,75,79,83,87,91,95]  
+}
+
+
+var['data'].append(config)
+print(var)
