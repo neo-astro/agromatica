621678ad89ba13ce512513546e5d646063e0197b
neo-astro <estevanabril@hotmail.es>
2023-06-24 18:39:52 -0500

    grafi1cas

--- utils/helpers.py
+++ utils/helpers.py
@@ -28,16 +28,14 @@ def crearPdf(fecha,registros):
         'margin-bottom': '0.1in',
         'margin-left': '0.1in'
     }
-    config = {
-        'distancia': [3,7,11,15,19,23,27,31,35,39,43,47,51,55,59,63,67,71,75,79,83,87,91,95]  
-    }
+    config = [3,7,11,15,19,23,27,31,35,39,43,47,51,55,59,63,67,71,75,79,83,87,91,95]  
     
     for registro in registros:
         if registro.get("fecha") == fecha:
             registro['distancia']= config
             html = template.render(registro)
             pdfkit.from_string(html,f'consulta_{fecha}.pdf',options=options)
-            
+
     # ,configuration=pdfkit.configuration(wkhtmltopdf=r'C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe')
 
 
