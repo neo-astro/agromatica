8c6f31091a6e21c97aac39c3c47ec7e07ad04628
neo-astro <estevanabril@hotmail.es>
2023-06-24 19:03:00 -0500

    grafi1cas

--- xd.py
+++ xd.py
@@ -62,6 +62,14 @@ config = {
     'distancia': [3,7,11,15,19,23,27,31,35,39,43,47,51,55,59,63,67,71,75,79,83,87,91,95]  
 }
 
+x =       {
+          "_id": "648e5b3d46448fa5c63d3828",
+          "fecha": "2023-10-10",
+          "senHumedadAire": {"1": 40,"2":50 },
+          "senHumedadAgua": {"1": 40,"2":50 },
+          "senPh": {"1": 40,"2":50 },
+          "senCalidadAire": {"1": 40,"2":50 }
+      }
 
 var['data'].append(config)
-print(var)
+print(len(x['senHumedadAgua']))
