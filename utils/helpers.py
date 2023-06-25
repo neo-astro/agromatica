ee5c9de05a5c3a0f2b494c07996d107fb801e61f
neo-astro <estevanabril@hotmail.es>
2023-06-24 21:36:02 -0500

    grafi1

--- utils/helpers.py
+++ utils/helpers.py
@@ -32,6 +32,12 @@ def crearPdf(fecha,var):
         'margin-left': '0.1in'
     }
 
+    
+    disAire = 3
+    disPh = 3
+    disHumedad = 3
+    disCalidad = 3
+
     for obj in var:
         new_registro = obj.copy()
         
